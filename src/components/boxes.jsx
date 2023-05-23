7661c237b6d9125ee2d1bb43da6c905c68866658
TayfunBekar <tayfunbekir@gmail.com>
2023-05-23 15:02:38 +0300

    css düzeltmeleri...

--- src/components/boxes.jsx
+++ src/components/boxes.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
- import '../styles/boxes.css';
- import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import '../styles/boxes.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faClock, faCoffee, faWater } from '@fortawesome/free-solid-svg-icons'
 
 
@@ -8,7 +8,6 @@ function Boxes() {
   return (
     
     <div id="services" class="services">
-    <h5> </h5>
     <h1>Hizmetlerimiz</h1>
     <div class="container3">
       <div class="box">
@@ -31,8 +30,6 @@ function Boxes() {
     </div>
     
   </div>
-
-
    
   );
 }

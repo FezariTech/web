970226710c0fca3c9338f11515825f34661b081b
TayfunBekar <tayfunbekir@gmail.com>
2023-05-25 16:11:26 +0300

    ...

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -7,8 +7,7 @@ function Body1() {
      <div class="container">
         <h1 class="title">Bizimle keşfedin!</h1>
         <div class="description">
-        <p>FezariTech, denizlerde ve iç sularda gözlem, ölçüm, veri toplama ve su altı haritalama çalışmaları için donanım ve yazılım çözümleri geliştirmektedir. 
-          Gözlem ve ölçüm faaliyetlerini otonom İDA teknolojilerimizle gerçekleştiriyor, maliyet, zaman ve iş gücü tasarrufu sağlıyoruz. </p>
+        <p>FezariTech, denizlerde ve iç sularda gözlem, ölçüm, veri toplama ve su altı haritalama çalışmaları için donanım ve yazılım çözümleri geliştirmektedir.</p>
             {/*<button class="rounded-shadow-button"> Keşfet...</button> */}    
         </div>
       </div>
--- src/components/boxes.jsx
+++ src/components/boxes.jsx
@@ -13,7 +13,7 @@ function Boxes() {
       <div class="box">
       <FontAwesomeIcon  icon={faClock}  className="my-icon"/>        
       <h2>Keşif ve Gözlem</h2>
-        <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown ...</p>
+        <p>Denizlerde ve iç sularda gözlem ve ölçüm faaliyetlerini otonom İDA teknolojilerimizle gerçekleştiriyor, maliyet, zaman ve iş gücü tasarrufu sağlıyoruz.</p>
         <ion-icon class="arrow" name="arrow-forward-outline"></ion-icon>
       </div>
       <div class="box">

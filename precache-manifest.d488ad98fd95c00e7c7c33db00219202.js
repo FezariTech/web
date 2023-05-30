5d61c91197b1d0076ddf0fa44c7b385330421752
TayfunBekar <tayfunbekir@gmail.com>
2023-05-30 15:49:47 +0300

    Updates

---
+++ precache-manifest.d488ad98fd95c00e7c7c33db00219202.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "e90fce6092c2e9c465a1",
+    "url": "/web/static/css/main.5439cc08.chunk.css"
+  },
+  {
+    "revision": "e90fce6092c2e9c465a1",
+    "url": "/web/static/js/main.e90fce60.chunk.js"
+  },
+  {
+    "revision": "a89266deb73d87b01859",
+    "url": "/web/static/js/1.a89266de.chunk.js"
+  },
+  {
+    "revision": "ccb52c7e1fe84eefecdb",
+    "url": "/web/static/js/2.ccb52c7e.chunk.js"
+  },
+  {
+    "revision": "0ca21152463244d07b43",
+    "url": "/web/static/js/runtime~main.0ca21152.js"
+  },
+  {
+    "revision": "8a0638503d80fc44c4b12474d1afe9a1",
+    "url": "/web/static/media/Teknofest_logo.8a063850.png"
+  },
+  {
+    "revision": "1adfdc1b25bfd0ba263d60438fd7452b",
+    "url": "/web/static/media/Karadeniz_Technical_University_logo.svg.1adfdc1b.png"
+  },
+  {
+    "revision": "88bcad12236a645438fdeda3d20516ee",
+    "url": "/web/static/media/TÜBİTAK_logo.svg.88bcad12.png"
+  },
+  {
+    "revision": "eeb229d997dfaf8f5fce36ce8f57535b",
+    "url": "/web/static/media/gemi3.eeb229d9.jpg"
+  },
+  {
+    "revision": "315408e86bc0a669e9cd76fbae3d0645",
+    "url": "/web/index.html"
+  }
+];
